86df86b76e9f66e21eeb3c98d6fe56c9498fadf3
jmauas <j@estudiomq.com.ar>
2022-07-30 19:20:16 -0300

    Entrega Final

--- Back/src/routes/productos.route.js
+++ Back/src/routes/productos.route.js
@@ -4,22 +4,22 @@ import * as prodController from '../controllers/productos.controllers.js';
 
 const router = express.Router();
 
-// router.get("/", auth, prodController.getProds);
-router.get("/", prodController.getProds);
+router.get("/", auth, prodController.getProds);
+//router.get("/", prodController.getProds);
 
-router.get("/:sku", prodController.getProd);
-// router.get("/:sku", auth, prodController.getProd);
+//router.get("/:sku", prodController.getProd);
+router.get("/:sku", auth, prodController.getProd);
 
-router.get("/categoria/:cat", prodController.getProdCat);
-// router.get("/categoria/:cat", auth, prodController.getProdCat);
+//router.get("/categoria/:cat", prodController.getProdCat);
+router.get("/categoria/:cat", auth, prodController.getProdCat);
 
-router.post("/", prodController.nuevoProd);
-// router.post("/", auth, prodController.nuevoProd);
+//router.post("/", prodController.nuevoProd);
+router.post("/", auth, prodController.nuevoProd);
 
-router.put("/:sku", prodController.modiProd);
-// router.put("/:sku", auth, prodController.modiProd);
+//router.put("/:sku", prodController.modiProd);
+router.put("/:sku", auth, prodController.modiProd);
 
-router.delete("/:sku", prodController.borrarProd);
-// router.delete("/:sku", auth, prodController.borrarProd);
+//router.delete("/:sku", prodController.borrarProd);
+router.delete("/:sku", auth, prodController.borrarProd);
 
 export default router;
